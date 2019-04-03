4326664288181209c4f4e5d25d5c1d6b03fd4d74
ALUNO <aluno@LADMAC0102.local>
2019-04-03 20:18:46 -0300

    Separação das Pastas do projeto no padrão MVC, e primeiro esboço das telas usando o balsamiq mockups

---
+++ CapacitacaoIOS-Descarte/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapacitacaoIOS-Descarte
+//
+//  Created by ALUNO on 03/04/19.
+//  Copyright © 2019 Aluno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
