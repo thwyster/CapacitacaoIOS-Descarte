8699b44b797e7cfd381f5386d1ac2cab19c16feb
ALUNO <aluno@LADMAC0102.local>
2019-04-10 21:52:01 -0300

    Implementação da mais algumas views e seus controllers no Storyboard

---
+++ CapacitacaoIOS-Descarte/Controller/CadastroController.swift
@@ -0,0 +1,17 @@
+//
+//  CadastroController.swift
+//  CapacitacaoIOS-Descarte
+//
+//  Created by ALUNO on 10/04/19.
+//  Copyright © 2019 Aluno. All rights reserved.
+//
+
+import UIKit
+
+public class CadastroController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ CapacitacaoIOS-Descarte/Controller/CadastroControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CadastroControllerViewController.swift
+//  CapacitacaoIOS-Descarte
+//
+//  Created by ALUNO on 10/04/19.
+//  Copyright © 2019 Aluno. All rights reserved.
+//
+
+import UIKit
+
+class CadastroControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
