a638326f93a403490ec58fe6c029d96b66e78425
ALUNO <aluno@LADMAC0102.local>
2019-04-12 21:03:50 -0300

    Implementado modificaçoes de telas, e algumas animações

---
+++ CapacitacaoIOS-Descarte/Controller/CadastroViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CadastroViewController.swift
+//  CapacitacaoIOS-Descarte
+//
+//  Created by ALUNO on 12/04/19.
+//  Copyright © 2019 Aluno. All rights reserved.
+//
+
+import UIKit
+
+class CadastroViewController: UIViewController {
+
+    @IBOutlet weak var containerViewColetor: UIView!
+    @IBOutlet weak var containerViewUsuario: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.containerViewUsuario.alpha = 1
+
+        self.containerViewColetor.alpha = 0
+        
+    }
+    
+    @IBAction func showComponent(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.title = "Usuario"
+                self.containerViewUsuario.alpha = 1
+                self.containerViewColetor.alpha = 0
+            })
+        }
+        else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.title = "Coletor"
+                self.containerViewUsuario.alpha = 0
+                self.containerViewColetor.alpha = 1
+            })
+        }
+    }
+}
