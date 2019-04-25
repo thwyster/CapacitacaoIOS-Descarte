91559bf96f0b0f8fb171311fae177e74610003cd
ALUNO <aluno@LADMAC0102.local>
2019-04-24 21:53:46 -0300

    Melhorando layout para iphone 7 Plus, criação das controllers dos containners

---
+++ CapacitacaoIOS-Descarte/Controller/CadastroUsuarioViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CadastroUsuarioViewController.swift
+//  CapacitacaoIOS-Descarte
+//
+//  Created by ALUNO on 24/04/19.
+//  Copyright © 2019 Aluno. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class CadastroUsuarioViewController: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtSenha: UITextField!
+    @IBOutlet weak var txtConfirmacaoSenha: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnSalvar(_ sender: Any) {
+        if (txtSenha.text! == txtConfirmacaoSenha.text!) {
+            Auth.auth().createUser(withEmail: txtEmail.text!, password: txtSenha.text!) { (result, error) in
+                guard (result?.user) != nil
+                    else
+                {
+                    print(error!)
+                    return
+                }
+                
+                print("LOGIN DEU BOA")
+            }
+        }
+        else {
+            print("LOGIN DEU RUIM!!!")
+        }
+    }
+
+}
